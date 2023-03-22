70a2d6e9aa4402e43a43b18e00cdfda4736774fa
whitgroves <whitney.groves@gmail.com>
2023-03-21 21:05:12 -0500

    prototype 1 complete

---
+++ outruntest/main.py
@@ -0,0 +1,146 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+import pygame as pg
+import sys
+import math
+
+TITLE = 'Floor It!'
+RESOLUTION = WIDTH, HEIGHT = 1024, 768
+HALF_HEIGHT = HEIGHT // 2
+FPS = 60
+ROAD_W = 2000
+SEG_L = 200
+CAM_D = 0.84
+MAX_SCALE = 5
+TEXTURES_PATH = 'outruntest/textures/'
+ROAD_LIGHT = pg.Color(107, 107, 107)
+ROAD_DARK = pg.Color(105, 105, 105)
+GRASS_LIGHT = pg.Color(16, 200, 16)
+GRASS_DARK = pg.Color(0, 154, 0)
+RUMBLE_LIGHT = pg.Color(255, 255, 255)
+RUMBLE_DARK = pg.Color(0, 0, 0)
+
+class Line:
+    def __init__(self) -> None:
+        self.x = self.y = self.z = 0.0 # world coordinates
+        self.X = self.Y = self.W = 0.0 # screen projection
+        self.scale = self.curve = self.sprite_X = self.clip = 0.0
+        self.sprite:pg.Surface = None
+        self.color_road:pg.Color = 'black'
+        self.color_grass:pg.Color = 'black'
+        self.color_rumble:pg.Color = 'black'
+        
+    def project(self, cam_x:int, cam_y:int, cam_z:int) -> None:
+        self.scale = CAM_D / (self.z - cam_z + 0.0001) # div/0 hazard
+        self.X = (1 + self.scale * (self.x - cam_x)) * WIDTH / 2
+        self.Y = (1 - self.scale * (self.y - cam_y)) * HEIGHT / 2
+        self.W = self.scale * ROAD_W * WIDTH / 2
+        
+    def draw_sprite(self, app:pg.Surface) -> None: # TODO: WHY?
+        if self.sprite is None: return
+        w = self.sprite.get_width()
+        h = self.sprite.get_height()
+        dest_X = self.X + self.scale * self.sprite_X * WIDTH / 2
+        dest_Y = self.Y + 4
+        dest_W = w * self.W / 266
+        dest_H = h * self.W / 266
+        dest_X += dest_W * self.sprite_X
+        dest_Y -= dest_H
+        clip_H = dest_Y + dest_H - self.clip
+        if clip_H < 0: clip_H = 0
+        if clip_H >= dest_H: return
+        if dest_W >= w: return
+        scaled = pg.transform.scale(self.sprite, (dest_W, dest_H))
+        cropped = scaled.subsurface(0, 0, dest_W, dest_H - clip_H)
+        app.blit(cropped, (dest_X, dest_Y))
+# endclass
+
+def draw_quad(w:pg.Surface, c:pg.Color, x1:int, y1:int, w1:int, x2:int, y2:int, w2:int) -> None:
+    pg.draw.polygon(surface=w, color=c, points=[(x1 - w1, y1),(x2 - w2, y2),(x2 + w2, y2),(x1 + w1, y1)])
+
+def get_texture(file:str, width:int = 0, height:int = 0) -> pg.Surface:
+        assert os.path.exists(TEXTURES_PATH)
+        texture = pg.image.load(os.path.join(TEXTURES_PATH, file)).convert_alpha()
+        x = texture.get_width() if width == 0 else width
+        y = texture.get_height() if height == 0 else height
+        return pg.transform.scale(texture, (x, y))
+
+class Game:
+    def __init__(self) -> None:
+        pg.init()
+        pg.display.set_caption(TITLE)
+        self.screen = pg.display.set_mode(RESOLUTION)
+        self.clock = pg.time.Clock()
+        # self.sprites:dict[str:pg.Surface] = {} # { filename : sprite }
+        self.sprite_bg:pg.Surface = get_texture('bg.png', width=WIDTH)
+        self.sprite_oak:pg.Surface = get_texture('oak.png')
+        self.lines:list[Line] = []
+        for i in range(1600):
+            line = Line()
+            line.z = i * SEG_L
+            color_switch = (i // 3) % 2
+            line.color_road = ROAD_LIGHT if color_switch else ROAD_DARK
+            line.color_grass = GRASS_LIGHT if color_switch else GRASS_DARK
+            line.color_rumble = RUMBLE_LIGHT if color_switch else RUMBLE_DARK
+            if 300 < i < 700: line.curve = 0.5
+            if i > 750: line.y = math.sin(i / 30.0) * 1500
+            if i > 1100: line.curve = -0.7
+            # TODO: multiple scenery sprites
+            if i % 20 == 0:
+                line.sprite_X = -2.5
+                line.sprite = self.sprite_oak
+            if i % 20 == 10:
+                line.sprite_X = 1.5
+                line.sprite = self.sprite_oak
+            self.lines.append(line)
+        self.N = len(self.lines)
+        self.N_scaled = self.N * SEG_L
+        self.pos = 0
+        self.player_x = 0
+        self.player_y = 1500
+    
+    def run(self) -> None:
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                    pg.quit()
+                    sys.exit()
+            # TODO: turbo
+            keys = pg.key.get_pressed()
+            if keys[pg.K_RIGHT]: self.player_x += 200
+            if keys[pg.K_LEFT]: self.player_x -= 200
+            if keys[pg.K_UP]: self.pos += 200
+            if keys[pg.K_DOWN]: self.pos -= 200
+            # TODO: camera adjustment
+            # TODO: fill below bg for higher camera angles
+            self.screen.blit(self.sprite_bg, (0, 0))
+            self.pos %= self.N_scaled
+            start_pos = self.pos // SEG_L
+            end_pos = start_pos + 300
+            x = dx = 0.0
+            cam_h = self.player_y + self.lines[start_pos].y
+            max_Y = HEIGHT
+            # TODO: turbo effect
+            # TODO: parallax
+            for n in range(start_pos, end_pos):
+                l = self.lines[n % self.N]
+                l.project(self.player_x - x, cam_h, self.pos - (self.N_scaled if n >= self.N else 0))
+                x += dx
+                dx += l.curve
+                l.clip = max_Y
+                if l.Y >= max_Y: continue
+                max_Y = l.Y
+                p = self.lines[(n - 1) % self.N]
+                draw_quad(self.screen, l.color_grass, 0, p.Y, WIDTH, 0, l.Y, WIDTH)
+                draw_quad(self.screen, l.color_rumble, p.X, p.Y, p.W * 1.2, l.X, l.Y, l.W * 1.2)
+                draw_quad(self.screen, l.color_road, p.X, p.Y, p.W, l.X, l.Y, l.W)
+            for n in range(end_pos, start_pos + 1, -1):
+                self.lines[n % self.N].draw_sprite(self.screen)
+            self.clock.tick(FPS)
+            pg.display.flip()
+            pg.display.set_caption(f'{TITLE} - FPS: {self.clock.get_fps():.1f}')
+# endclass
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
