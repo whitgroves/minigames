ce23330181a8a97fd085a42cc3474d8a869d82d3
whitgroves <whitney.groves@gmail.com>
2023-11-08 14:12:53 -0600

    breakout tutorial 6/10

--- tutorials/breakout/basics.js
+++ tutorials/breakout/basics.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("mainCanvas");
 const ctx = canvas.getContext("2d");
 
-// start: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
+// start (1/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
 
 // ctx.beginPath();
 // ctx.rect(20, 40, 50, 50);
@@ -21,7 +21,7 @@ const ctx = canvas.getContext("2d");
 // ctx.stroke();
 // ctx.closePath();
 
-// checkpoint: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Move_the_ball
+// checkpoint (2/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Move_the_ball
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -45,7 +45,7 @@ let dy = -2;
 // }
 // setInterval(draw, 10);
 
-// checkpoint: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
+// checkpoint (3/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
 
 const ballRadius = 10;
 
@@ -71,7 +71,7 @@ const ballRadius = 10;
 // }
 // setInterval(draw, 10);
 
-// checkpoint: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Paddle_and_keyboard_controls
+// checkpoint (4/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Paddle_and_keyboard_controls
 
 const paddleHeight = 10;
 const paddleWidth = 75;
@@ -144,13 +144,82 @@ function drawPaddle() {
 // }
 // setInterval(draw, 10);
 
-// checkpoint: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Game_over
+// checkpoint (5/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Game_over
 
 const interval = setInterval(draw, 10);
+
+// function draw() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     drawPaddle();
+//     drawBall();
+
+//     if (rightPressed) {
+//         paddleX = Math.min(paddleX + paddleSpeed, canvas.width - paddleWidth);
+//     } else if (leftPressed) {
+//         paddleX = Math.max(paddleX - paddleSpeed, 0);
+//     }
+
+//     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+//         dx = -dx;
+//     }
+
+//     if (y + dy < ballRadius) {
+//         dy = -dy;
+//     } else if (y + dy > canvas.height - ballRadius) {
+//         if (x > paddleX && x < paddleX + paddleWidth) {
+//             dy = -dy;
+//             dx *= 1.1;
+//             dy *= 1.1;
+//         } else {
+//             alert("Game over, man, game over!")
+//             document.location.reload();
+//             clearInterval(interval); // ends game in chrome
+//         }
+//     }
+
+//     x += dx;
+//     y += dy;
+// }
+
+// checkpoint(6/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Build_the_brick_field
+
+const brickRowCount = 4;
+const brickColCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 20;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+for (let col = 0; col < brickColCount; col++) {
+    bricks[col] = [];
+    for (let row = 0; row < brickRowCount; row++) {
+        bricks[col][row] = { x: 0, y: 0 };
+    }
+}
+
+function drawBricks() {
+    for (let col = 0; col < brickColCount; col++) {
+        for (let row = 0; row < brickRowCount; row++) {
+            const brickX = col * (brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = row * (brickHeight + brickPadding) + brickOffsetTop;
+            bricks[col][row].x = 0;
+            bricks[col][row].y = 0;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = defaultColor;
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    drawBricks();
     drawPaddle();
     drawBall();
 
@@ -182,4 +251,4 @@ function draw() {
     y += dy;
 }
 
-// checkpoint: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Build_the_brick_field
+// checkpoint (7/10): https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Collision_detection
